328ab61eeb7e28f59bd21c59e3203b6f1c84b834
minhazapon <minhajulabedinapon@gmail.com>
2024-07-17 22:15:40 -0700

    first commit

---
+++ op.js
@@ -0,0 +1,78 @@
+
+
+
+
+// arithmetic
+
+
+const num = 20;
+
+const num2 = 200;
+
+const allNum = num + num2;
+// console.log(allNum)
+
+const run = 200;
+
+const run2 = 300;
+// console.log(run - run2)
+
+const ban = 500;
+
+const ind = 1000;
+const BI = ban * ind
+// console.log(BI)
+
+const apon = 1000;
+const minhaz = 2000;
+const allName = apon / minhaz
+// console.log(allName);
+
+const js = 10;
+const react = 50;
+const language = js % react
+// console.log(language)
+
+
+// relation operator
+
+
+let reactJs = 50;
+let nodeJS = 100;
+
+let allJs = nodeJS > reactJs;
+// console.log(allJs)
+
+let RN = nodeJS < reactJs;
+// console.log(RN);
+
+let jsTalk = nodeJS <= reactJs
+// console.log(jsTalk)
+
+let jsHand = reactJs <= nodeJS
+// console.log(jsHand)
+
+let jsBangladesh = nodeJS == reactJs
+// console.log(jsBangladesh)
+
+let jsTech = nodeJS === reactJs
+// console.log(jsTech);
+
+
+//logical operator
+
+// 1. and operator
+// 2. or operator
+
+const quota = 200;
+const quota2 = 300;
+
+const logical = (quota > 400) && (quota2 < 1000);
+// console.log(logical);
+
+const logical2 = (quota > 150) || (quota2 < 10);
+// console.log(logical2)
+
+
+
+
