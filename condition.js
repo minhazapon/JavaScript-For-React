a41959e876c618723e4e188ac8899471a5914785
minhazapon <minhajulabedinapon@gmail.com>
2024-07-26 23:31:37 -0700

    added file

---
+++ condition.js
@@ -0,0 +1,164 @@
+
+
+const minhaz = 23;
+
+const orin  = 10;
+
+
+if ( minhaz > orin ){
+
+    console.log('minhaz orin er theke boro')
+
+} else {
+
+    console.log('orin apon er theke choto')
+
+
+}
+
+
+// ----------------------------------------------
+
+
+
+
+const alamin = 23;
+
+const apon = 23;
+
+
+if ( alamin > apon ){
+
+    console.log('apon ar alamin age ek')
+
+} else if( alamin >= apon  ){
+
+    console.log('both are same')
+
+} else {
+
+    console.log(' they are same ')
+}
+
+
+
+if ( alamin > apon  ){
+
+    console.log('apon ar al amin same')
+} else if (alamin < apon){
+
+    console.log('apon alamin er theke boro')
+} else {
+
+    console.log('apon alamin are same')
+}
+
+// ------------------------------------------------
+
+
+
+const mangoPrice = 500;
+
+const jackFrutPrice = 600;
+
+
+if ( mangoPrice > jackFrutPrice ) {
+
+    console.log('mango is tasty')
+} else {
+
+    console.log('jack frut is tasty then mango')
+}
+
+
+if ( mangoPrice >= jackFrutPrice ){
+  console.log('is true')
+} else {
+    console.log('mango khabo')
+}
+
+
+
+const mutton = 700;
+
+const beef = 700;
+
+if( mutton > beef ){
+   
+    console.log('mutton er theke beef beshi moja')
+
+} else if( beef > mutton  ){
+     
+    console.log(' mutton kom moja ')
+  
+} else {
+
+    console.log('meat are same of us')
+
+}
+
+
+
+const bnp = 1000;
+
+const awmilig = 2000;
+
+if( bnp > awmilig ){
+   
+    console.log('bnp awmilig er theke ekhon beshi khomotaban')
+
+} else {
+
+    console.log('awmilig boro')
+}
+
+
+const javaScript = 80000;
+
+const python = 80000;
+
+
+if ( javaScript > python ){
+
+    console.log('js bigger than py')
+} else if( python > javaScript ){
+
+    console.log('python bigger than js')
+} else {
+
+    console.log('js and python are same')
+}
+
+
+
+
+const love  = 3000;
+
+const hate = 3000;
+
+if( love > hate  ){
+
+    console.log('love is great')
+
+} else {
+
+    console.log('hate is not good')
+}
+
+
+
+
+if( love > hate ){
+
+    console.log('love is good')
+
+
+} else if( hate > love ){
+
+
+    console.log('hate is good')
+} else {
+
+    console.log('love and hate both are same')
+}
+
