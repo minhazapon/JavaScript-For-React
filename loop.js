b53f290c0a49299106a6d68ec8eb4741239b49b2
minhazapon <minhajulabedinapon@gmail.com>
2024-07-27 21:41:29 -0700

    added files

---
+++ loop.js
@@ -0,0 +1,148 @@
+
+
+
+
+// for loop
+
+
+ let apon = 0
+
+
+for( let i = 1 ; i < 10; i++ ){
+
+   apon = apon + i
+
+   console.log('apon', apon , 'i =' , i, 'and ', apon+i)
+
+
+}
+
+console.log('apon', apon)
+
+
+
+
+ let minhaz = 0 ;
+
+
+ for( let i = 2; i < 50; i++ ){
+
+   minhaz = minhaz + i
+
+   console.log( 'minhaz =', minhaz, 'i =', i, 'and', minhaz + i )
+ 
+
+ }
+
+
+ console.log('minhazul abedin apon loop result is a ', minhaz)
+
+
+
+ let mangoPrice = 10;
+
+
+ for(let i = 5; i < 30; i++){
+
+      
+    mangoPrice = mangoPrice + i;
+
+    console.log('mangoPrice', mangoPrice, 'i = ', i, 'and', mangoPrice+i)
+
+
+
+}
+
+
+ console.log('mangoAllPrice', mangoPrice)
+
+
+ let beefPrice = 10;
+
+
+ for(let i = 5; i < 40; i++){
+
+    
+     beefPrice = beefPrice + i
+
+      console.log('beef = ', beefPrice, 'i = ', i, 'allPrice', beefPrice+i);
+
+
+ }
+
+
+  console.log('AllBeefPrice', beefPrice);
+   
+
+
+   let waterPrice = 10;
+
+   for( let i = 5; i < 50; i++ ){
+
+     waterPrice = waterPrice * i;
+
+     console.log('water = ', waterPrice , 'i = ', i , ' allPrice = ', waterPrice+i)
+
+   }
+
+    console.log('waterPriceAll', waterPrice)
+
+
+
+
+
+
+
+ //WHILE LOOP...................................................
+
+
+
+  let reactPrice = 10;
+
+//   let i = 20;
+
+  while( i < 50 ){
+
+    reactPrice = reactPrice + i
+
+    i++
+
+
+  }
+
+
+  console.log(reactPrice);
+
+
+
+   let pakistanPrice = 10;
+
+   let i = 100;
+
+   while(i < 200){
+
+      pakistanPrice = pakistanPrice + i
+
+      i++
+
+
+   }
+
+   console.log('allPrice', pakistanPrice)
+
+
+   let seaPrice = 10;
+
+   let pon = 30;
+
+   while(pon < 1000){
+
+      seaPrice = seaPrice + pon
+
+      pon++
+
+
+   }
+
+   console.log('pon = ', seaPrice);
+
