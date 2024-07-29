1e7ad22b861c49947aeed5c5bc96205d49ac2a35
minhazapon <minhajulabedinapon@gmail.com>
2024-07-28 22:06:18 -0700

    array file

---
+++ array.js
@@ -0,0 +1,52 @@
+
+// Basic Array
+
+
+    const tech = ['html', 'css', 'javaScript', 'react.js', 'next.js', 'DSA', 'node.js', 'postgreSQL' ];
+    
+    console.log(tech[4]);
+    console.log(tech[3]);
+    console.log(tech[7]);
+    console.log(tech[5]);
+    console.log('apon tech skills = ', tech[3],[5],[7] )
+
+    tech [8] = 'mongodb'
+
+    console.log(tech);
+    
+
+
+    const frutPrice = ['mango=200', 'kathal=300', 'angur=1000', 'komla=500', 'banggi=400']
+
+
+    frutPrice [5] = 'kola=100'
+    frutPrice [6] = 'jam=200'
+    frutPrice [7] = 'ashfol=100'
+    frutPrice [8] = 'pepe=800'
+    frutPrice [9] = 'panifol=150'
+    frutPrice [10] = 'licu=1500'
+ 
+    console.log(frutPrice.length);
+    console.log(frutPrice[9]);
+    console.log(frutPrice);
+
+
+
+    const politicalParties = [ 'bnp', 'awamilig', 'jamat e islami', 'jatiyo party' ]
+    
+    politicalParties [4] = 'gono odhikar party'
+    politicalParties [5] = 'apon party'
+    
+    console.log(politicalParties);
+    console.log(politicalParties.length);
+    console.log(politicalParties[0])
+    console.log(politicalParties);
+    politicalParties.push('minhazul abedin apon')
+    console.log(politicalParties)
+    politicalParties.pop()
+    console.log(politicalParties);
+
+
+
+
+
