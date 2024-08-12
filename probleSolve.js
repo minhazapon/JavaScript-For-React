ffc25a4d515fbb37944a6644d38eb065d48349e6
minhazapon <minhajulabedinapon@gmail.com>
2024-08-11 22:36:21 -0700

    array problem

---
+++ probleSolve.js
@@ -0,0 +1,62 @@
+
+
+//1no. problem 
+
+const frutsArray = ['mango', 'jam', 'kathal', 'apple', 'pepe', 'lichu']
+
+console.log(frutsArray[3])
+
+const frutszTwo = frutsArray[2] = 'angur'
+
+console.log(frutsArray);
+
+
+
+//2no. problem
+
+
+const turistHub = ['coxBazar', 'bandorban', 'kuakata', 'kashmir']
+
+
+turistHub.push('syleth')
+
+turistHub.push('sazek', 'rangamatee')
+
+turistHub.pop();
+
+console.log(turistHub);
+
+
+//3no. problem
+
+
+const Book = ['aponWithReact', 'JavaScriptMama', 'chatroAndolon', 'aponsDream', 'hateKolomeJavaScript']
+
+
+console.log(Book.includes('hateKolomeJavaScript'))
+
+console.log(Book);
+
+
+
+//4no. problem 
+
+const aponSkills = ['react.js', 'javaScript', 'DSA']
+
+console.log(Array.isArray(aponSkills));
+
+
+//5no. problem
+
+
+const frontEnd = ['react.js', 'next.js', 'tailwind']
+
+const backEnd = ['node.js', 'express.js', 'postgreSQL']
+
+// frontEnd.concat(backEnd)
+
+console.log(frontEnd.concat(backEnd));
+
+
+
+
