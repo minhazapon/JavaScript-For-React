0ea1fc62e2627296cce7d18b12101c5f1d92207d
minhazapon <minhajulabedinapon@gmail.com>
2024-08-12 22:30:47 -0700

    array Problems

--- arrayProblems.js
+++ arrayProblems.js
@@ -59,4 +59,49 @@ console.log(frontEnd.concat(backEnd));
 
 
 
+//6no. problem
 
+const colors = ['red', 'blue', 'green', 'yellow', 'orange']
+
+
+      colors.reverse()
+      console.log(colors)
+
+
+
+//7no. problem
+
+
+const numbers = [12, 98, 5, 41, 23, 78, 46];
+
+
+for( i = 0; i < numbers.length; i++ ){
+   
+    console.log(numbers[i])
+    
+}
+
+
+//8no problem
+
+
+const namz = ['Tom', 'Tim', 'Tin', 'Tik']
+
+
+for( i = 0; i < namz.length; i++ ){
+
+   console.log(namz[i].concat(namz))
+
+}
+
+
+//9no problem
+
+
+
+     const statement = ['i', 'am', 'a', 'hard', 'working', 'person']
+
+     statement.reverse()
+     console.log(statement)
+
+     
---
+++ arrayTraver.js
@@ -0,0 +1,191 @@
+
+
+
+
+const bangladesh = ['jhenaidah', 'khulna', 'jashore', 'kushtia', "chuadanga", 'razbaree']
+
+
+
+for( i = 0; i < bangladesh.length; i++ ){
+
+    console.log(bangladesh[i])
+
+
+}
+
+
+const aponFrndz = ['shemanto', 'ovi', 'omi', 'dip', 'alamin', 'sopnil']
+
+
+for( i = 0; i < aponFrndz.length; i++ ){
+
+     console.log(aponFrndz[i])
+
+
+}
+
+
+
+//revers -----------------------------------------------------
+
+
+
+
+const numberz = [12, 23, 455, 456 ,3453, 23545,345, 3545, 56];
+
+
+  numberz.reverse()
+
+  console.log(numberz);
+
+
+
+const aponSkills = ['javaScript', 'react.js', 'DSA', 'next.js', 'postgreSQL']  
+
+
+   aponSkills.reverse()
+
+   console.log(aponSkills)
+
+
+
+const frutz = ['mango', 'jackFrut', 'pepe', 'dalim', 'jam', 'anggur', 'banggi', 'kola']
+
+
+   frutz.reverse()
+
+   console.log(frutz);
+
+
+
+const river = ['podda', 'meghna', 'jamuna', 'gorai', 'rupsha', 'navabgannga']
+
+
+   river.reverse()
+
+   console.log(river);
+
+
+
+
+
+// javaScript sort------------------------------------------------------------
+
+
+const num = [3, 5, 6, 8, 9, 1, 2,  ]
+
+
+    num.sort()
+     
+    console.log(num);
+
+
+    
+const aponStack = ['react.js', 'DSA', 'next.js', 'postgreSQl', 'JavaScript']
+
+
+     aponStack.sort()
+
+     console.log(aponStack);
+
+
+const minhazDreams = ['google', 'toptal', 'amazon', 'facebook', 'ollyo']
+
+
+     minhazDreams.sort()
+
+     console.log(minhazDreams)
+
+
+
+
+
+
+// array traverz----------------------------     
+
+
+ const aponSkillz = ['react.js', 'DSA', 'next.js', 'postGreSQL']
+
+
+ for( i = 0; i < aponSkillz.length; i++ ){
+
+    
+    console.log(aponSkillz[i])
+
+
+ }
+
+
+ const companiez = ['toptal', 'facebook', 'amazon', 'google', 'tcs', 'BrainStation', 'ollyo']
+
+ 
+ for( i = 0; i < companiez.length; i++ ){
+
+     console.log(companiez[i])
+
+ }
+
+
+
+ const banglaCompaniez = ['BrainStation', 'ollyo', 'DataSoft'];
+
+     
+ for( i = 0; i < banglaCompaniez.length; i++ ){
+
+     console.log(banglaCompaniez[i])
+
+ }
+
+
+
+
+//array revers----------------------------------------------------
+
+
+
+   const apoNumberz = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+         apoNumberz.reverse()
+         console.log(apoNumberz)
+   
+
+   const apoSoft = ['react.js', 'DSA', 'next.js', 'postgreSQL', 'Node.js', 'express.js', "mongoDB", 'mernStack']
+   
+         
+         apoSoft.reverse()
+         console.log(apoSoft)
+
+   const Districz = ['rajshahi', 'chapai', 'rangpur', 'bogura', 'pochogarh']
+   
+   
+        Districz.reverse()
+        console.log(Districz);
+
+
+        
+//arraySort-------------------------------------------------------------------
+
+
+
+
+    const minhazNumberz = [2, 4, 5, 6,3, 8, 9]
+
+         minhazNumberz.sort()
+         console.log(minhazNumberz)
+
+
+    const abedinApon = [ 'JavaScript', 'react.js', 'DSA', 'next.js' ]     
+
+
+          abedinApon.sort()
+          console.log(abedinApon)
+
+
+    const GoodFood = ['biriyani', 'beef', 'beefChap', 'nanGril', 'kalaBhuna']
+    
+    
+          GoodFood.sort()
+          console.log(GoodFood);
+
+
+          
