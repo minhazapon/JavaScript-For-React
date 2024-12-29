5ea1349785a74000efacc686a2b473adb479c0ca
minhazapon <minhajulabedinapon@gmail.com>
2024-12-28 20:04:08 -0800

    added file

--- Data_types.js
+++ Data_types.js
@@ -1,70 +1,132 @@
 
 
-///string////
-
-const name1 = 'minhazul abedon apon';
-const name2 = 'full Stack Developer';
-
-console.log(name1, name2)
-
-
-//numeric////
-
-
-const num1 = '12'
-const num2 = '32'
-const num3 = '40'
-const allNumZ = num1 + num2 + num3; 
-console.log(allNumZ);
-
-//bolean///
-
-
-const aponIsjavaScriptDeveloper = true; 
-const aponInPythonDeveloper = false 
-console.log(aponInPythonDeveloper, aponIsjavaScriptDeveloper); 
-
-
-///parseFloat and parsInt///
-
-const mango = 800;
-const angur = 900;
-const law = 300;
-const jam = 500;
-const lichu = 600;
-const kathal = parseFloat(300.400);
-const anaros = parseInt(1000);
-
-const AllFrust = mango + anaros + law + jam + lichu + kathal + anaros 
-
-const Frutz = AllFrust / 300;
-
-console.log(Frutz);
-
-
-
-///arith Matic//////
-
-
-const nodejs = 300;
-
-const reactjs = 400;
-
-const mongoDB = 500;
-
-const nextJs = 100;
-
-const t1 = nodejs + reactjs + mongoDB + nextJs; 
-const t2 = nodejs / reactjs / mongoDB / nextJs;
-const t3 = nodejs * reactjs * mongoDB * nextJs; 
-const t4 = nodejs - reactjs - mongoDB - nextJs; 
-
-const account = t1 + t2 + t3 +t4 
-console.log(account)
-
-
+    ///string////
+    
+    const name1 = 'minhazul abedon apon';
+    const name2 = 'full Stack Developer';
+    
+    console.log(name1, name2)
+    
+    
+    //numeric////
+    
+    
+    const num1 = '12'
+    const num2 = '32'
+    const num3 = '40'
+    const allNumZ = num1 + num2 + num3; 
+    console.log(allNumZ);
+    
+    //bolean///
+    
+    
+    const aponIsjavaScriptDeveloper = true; 
+    const aponInPythonDeveloper = false 
+    console.log(aponInPythonDeveloper, aponIsjavaScriptDeveloper); 
+    
+    
+    ///parseFloat and parsInt///
+    
+    const mango = 800;
+    const angur = 900;
+    const law = 300;
+    const jam = 500;
+    const lichu = 600;
+    const kathal = parseFloat(300.400);
+    const anaros = parseInt(1000);
+    
+    const AllFrust = mango + anaros + law + jam + lichu + kathal + anaros 
+    
+    const Frutz = AllFrust / 300;
+    
+    console.log(Frutz);
+    
+    
+    
+    ///arith Matic//////
+    
+    
+    const nodejs = 300;
+    
+    const reactjs = 400;
+    
+    const mongoDB = 500;
+    
+    const nextJs = 100;
+    
+    const t1 = nodejs + reactjs + mongoDB + nextJs; 
+    const t2 = nodejs / reactjs / mongoDB / nextJs;
+    const t3 = nodejs * reactjs * mongoDB * nextJs; 
+    const t4 = nodejs - reactjs - mongoDB - nextJs; 
+    
+    const account = t1 + t2 + t3 +t4 
+    console.log(account)
 
 
+    //////////////////////////data types///////////////////////////////////////
+
+    //string
+
+    const tech1 = 'javaScript';
+    const tech2 = 'typeScript';
+    const tech3 = 'react.js';
+    const tech4 = 'next.js';
+    const tech5 = 'redux';
+    const minhazSkills = { tech1, tech2, tech3, tech4, tech5 }
+     
+
+    console.log(minhazSkills.tech1, minhazSkills.tech2, minhazSkills.tech3, minhazSkills.tech4,minhazSkills.tech5, 'DSA');
+
+
+    
+    //number
+    
+    const n1 = 1950374409
+    const n2 = 78666778 
+    const n3 = 23498
+    const n4 = 2233456 
+    const allN = n1 + n2 + n3 + n4 
+
+    console.log(allN)
+
+    ///boolean
+
+
+    const aponInMinister = false 
+    const aponInKiller = false 
+    const aponInMoutushi = true 
+    
+    console.log(aponInKiller, aponInMinister, aponInMoutushi)
+
+
+    ////parseFloat and parseInt
+
+
+    const mac = 800 
+    const applePhone = 800
+    const HP = 1000 
+    const thinkPad = parseFloat(999.111111111)
+    const sumsung = parseInt(1000.234)
+
+    console.log(mac, applePhone, HP, thinkPad, sumsung)
+
+
+    ///arith Matic
+
+
+    const bangladesh = 7000 
+    const india = 10000 
+    const pakistan = 8000 
+    const afganistan = 90000 
+    const turkey = 80000 
+
+    console.log(bangladesh + india + pakistan + afganistan + turkey)
+    console.log(bangladesh - india - pakistan - afganistan - turkey)
+    console.log(bangladesh / india / pakistan / afganistan / turkey)
+    console.log(bangladesh * india * pakistan * afganistan * turkey)
+
+
+    
 
 
 
--- array.js
+++ array.js
@@ -1,5 +1,5 @@
 
-// Basic Array
+    // Basic Array
 
 
     const tech = ['html', 'css', 'javaScript', 'react.js', 'next.js', 'DSA', 'node.js', 'postgreSQL' ];
@@ -49,153 +49,204 @@
 
 
 
-// ----------------------------------------------------
+    // ----------------------------------------------------
+
+
+    //basic array--
+    
+    
+    const names = ['fahim', 'apon', 'rinu', 'shemanto', 'irfan', 'ovi', 'prottoy']
+    
+    console.log(names);
+    
+    const party = ['bnp', 'jamat', 'jatio party', 'chormonai', "gonoodhikar", ]
+    
+    console.log(party);
+    
+    const jhenaidaBnp = ['moshiur rahman', 'mozid', 'choto mozid', 'rahmat vai', 'somin vai']
+    console.log(jhenaidaBnp);
+    
+    
+    const techLanguages = ['javaScript', 'python', 'java', 'C++', 'c##', 'dart', 'go', 'rubay']
+    
+    console.log(techLanguages);
+    
+    
+    
+    // lenght -------------------------------------------------------
+    
+    
+    const numbers = [12, 344, 4545, 234, 45 ,45, 55 ,]
+    
+    console.log(numbers.length);
+    
+    const river = ['podda', 'meghna', 'jamuna', 'gorai', 'rupsha', 'navaganga']
+    
+    console.log(river.length);
+    
+    const districs = ['jhenaida', 'jashore', 'kushtia', 'meherpur', 'chuadanga' ]
+    
+    console.log(districs.length);
+    
+    
+    // index -------------------------------------------------------------
+    
+    const javaScriptTech = ['react.js', 'node.js', 'postgreSQL', 'express.js']
+    
+    console.log(javaScriptTech[3]);
+    
+    const pythonTech = ['flex', 'DJango']
+    
+    console.log(pythonTech[1]);
+    
+    const indiaPlace = ['kashmir', 'manali', 'ladakh', 'jammu', 'karela', 'kolkata', 'mumbai']
+    
+    console.log(indiaPlace[4]);
+    
+    const islamiCountry = ['afganistan', 'bangladesh', 'pakistan', 'saudi arabia', 'turkey']
+    
+    console.log(islamiCountry[4])
+    
+    
+    // push------------------------------
+    
+    
+    const bangladesh = ['dhaka', 'chittagong', 'khulna', 'rangpur', 'syleth'];
+    
+    bangladesh.push('barishal')
+    
+    console.log(bangladesh)
+    
+    
+    const jhenaida = ['jhenaida sadar', 'kaliganz', 'mmohespur', ]
+    
+    jhenaida.push('kotchadpur', 'shoilokupa');
+    
+    console.log(jhenaida);
+    
+    
+    const aponSkills = ['DSA']
+    
+    aponSkills.push('react.js', 'javaScript')
+    console.log(aponSkills)
+    
+    
+    //pop---------------------------------------------------
+    
+    const bongoParties = ['bnp', 'jamat', 'jatio party',  ]
+    
+    bongoParties.pop();
+    
+    console.log(bongoParties);
+    
+    const aponFamily = ['apon', 'orin', 'ohid', 'mini', 'dhini']
+    
+    aponFamily.pop();
+    
+    console.log(aponFamily)
+    
+    
+    //includes---------------------------------------------------------
+    
+    
+    const frutsName = ['mango', 'apple', 'jackFruts', 'pepe', 'khajoor']
+    
+    console.log(frutsName.includes('khajoor'));
+    
+    const countryNames = ['bangladesh', 'india', 'pakistan', 'myanmar', 'america']
+    
+    console.log(countryNames.includes('pakistan'))
+    
+    
+    console.log(Array.isArray(frutsName));
+    
+    const apon = 100;
+    
+    console.log(Array.isArray(apon))
+    
+    
+    
+    
+    const aponTech = [ 'javaScript', 'typeScript', 'react.js', 'next.js', 'redux', 'nodejs', 'expressJs', 'mongoDB', 'mongoose', 'graphQl', 'AWS', 'docker' ];
+    
+    console.log(aponTech);
+    
+    console.log(aponTech[4]);
+    
+    console.log(aponTech[8]);
+    
+    console.log(aponTech[2]);
+    
+    console.log(aponTech[3]);
+    
+    
+    aponTech[9] = 'html';
+    aponTech[8] = 'css';
+    console.log(aponTech);
+    
+    
+    const aponStack = [ 'frontEND DEVELOPER', 'backend developer', 'mernStack developer', 'full stack engineers' ]
+    
+    console.log(aponStack);
+    
+    aponStack[4] = 'aws engineers';
+    aponStack[5] = 'software developer'
+    
+    console.log(aponStack);
+    console.log(aponStack.length)
 
 
-//basic array--
 
+     
+    /////array////
 
-const names = ['fahim', 'apon', 'rinu', 'shemanto', 'irfan', 'ovi', 'prottoy']
 
-console.log(names);
+    //basic///
 
-const party = ['bnp', 'jamat', 'jatio party', 'chormonai', "gonoodhikar", ]
 
-console.log(party);
+    const sea = [ 'deep sea', 'atlantic sea', 'bay of bengal', 'red sea' ] 
 
-const jhenaidaBnp = ['moshiur rahman', 'mozid', 'choto mozid', 'rahmat vai', 'somin vai']
-console.log(jhenaidaBnp);
+    console.log(sea)
+    console.log(sea[3]) 
+    console.log(sea[0])
+    console.log(sea[1])
+    console.log(sea[2])
 
 
-const techLanguages = ['javaScript', 'python', 'java', 'C++', 'c##', 'dart', 'go', 'rubay']
+    //array in length////
 
-console.log(techLanguages);
 
+    const allNumbers = [ 12, 234, 35, 566, 255, 75, 35, 355, 256 ] 
 
+    console.log(allNumbers.length)
 
-// lenght -------------------------------------------------------
 
+    ///array in push///
 
-const numbers = [12, 344, 4545, 234, 45 ,45, 55 ,]
 
-console.log(numbers.length);
+    const allRiver = [ 'podda', 'meghna', 'jamuna', 'mathavanga' ] 
 
-const river = ['podda', 'meghna', 'jamuna', 'gorai', 'rupsha', 'navaganga']
+    allRiver.push('gorai, modhumoti') 
 
-console.log(river.length);
+    console.log(allRiver)
 
-const districs = ['jhenaida', 'jashore', 'kushtia', 'meherpur', 'chuadanga' ]
 
-console.log(districs.length);
+    ////array in pop//
 
+    const allParty = [ 'bnp', 'jamat', 'awmilig', 'jatioparty' ]
 
-// index -------------------------------------------------------------
+    allParty.pop([2]) 
+    allParty.pop()
 
-const javaScriptTech = ['react.js', 'node.js', 'postgreSQL', 'express.js']
+    console.log(allParty)
 
-console.log(javaScriptTech[3]);
+    
+    ////includes
 
-const pythonTech = ['flex', 'DJango']
+    const tourPlaces = [ 'kashmir', 'kolkata', 'mumbai', 'karimganz', 'saudi arab', 'south india' ] 
 
-console.log(pythonTech[1]);
 
-const indiaPlace = ['kashmir', 'manali', 'ladakh', 'jammu', 'karela', 'kolkata', 'mumbai']
-
-console.log(indiaPlace[4]);
-
-const islamiCountry = ['afganistan', 'bangladesh', 'pakistan', 'saudi arabia', 'turkey']
-
-console.log(islamiCountry[4])
-
-
-// push------------------------------
-
-
-const bangladesh = ['dhaka', 'chittagong', 'khulna', 'rangpur', 'syleth'];
-
-bangladesh.push('barishal')
-
-console.log(bangladesh)
-
-
-const jhenaida = ['jhenaida sadar', 'kaliganz', 'mmohespur', ]
-
-jhenaida.push('kotchadpur', 'shoilokupa');
-
-console.log(jhenaida);
-
-
-const aponSkills = ['DSA']
-
-aponSkills.push('react.js', 'javaScript')
-console.log(aponSkills)
-
-
-//pop---------------------------------------------------
-
-const bongoParties = ['bnp', 'jamat', 'jatio party',  ]
-
-bongoParties.pop();
-
-console.log(bongoParties);
-
-const aponFamily = ['apon', 'orin', 'ohid', 'mini', 'dhini']
-
-aponFamily.pop();
-
-console.log(aponFamily)
-
-
-//includes---------------------------------------------------------
-
-
-const frutsName = ['mango', 'apple', 'jackFruts', 'pepe', 'khajoor']
-
-console.log(frutsName.includes('khajoor'));
-
-const countryNames = ['bangladesh', 'india', 'pakistan', 'myanmar', 'america']
-
-console.log(countryNames.includes('pakistan'))
-
-
-console.log(Array.isArray(frutsName));
-
-const apon = 100;
-
-console.log(Array.isArray(apon))
-
-
-
-
-const aponTech = [ 'javaScript', 'typeScript', 'react.js', 'next.js', 'redux', 'nodejs', 'expressJs', 'mongoDB', 'mongoose', 'graphQl', 'AWS', 'docker' ];
-
-console.log(aponTech);
-
-console.log(aponTech[4]);
-
-console.log(aponTech[8]);
-
-console.log(aponTech[2]);
-
-console.log(aponTech[3]);
-
-
-aponTech[9] = 'html';
-aponTech[8] = 'css';
-console.log(aponTech);
-
-
-const aponStack = [ 'frontEND DEVELOPER', 'backend developer', 'mernStack developer', 'full stack engineers' ]
-
-console.log(aponStack);
-
- aponStack[4] = 'aws engineers';
- aponStack[5] = 'software developer'
-
-console.log(aponStack);
-console.log(aponStack.length)
-
-
+    console.log(tourPlaces.includes('kashmir')) 
 
 
 
